c517a4aa67949892dfc6672f2d1c63df883be0b7
Girija Shankar Mohanta <107687722+Girijamohantashankar@users.noreply.github.com>
2023-02-27 09:37:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+var flag = 0;
+$(document).ready(function () {
+    $('#burger').click(function () {
+        console.log("click")
+        $('.navbar').toggleClass('show');
+    })
+
+    $('#burger').click(function () {
+        $('.la-bars').toggleClass('la-times');
+    })
+})
+
+
+
+// <===========================================Dark mode Setting===========================================>
+
+
+$(function () {
+    $(".setting").on("click", function () {
+      $(this).parent().toggleClass("show");
+    });
+    $(".side__list__item").on("click", function () {
+      $(this).toggleClass("active");
+      var value = $(this).attr("data-text");
+      $("body").toggleClass(value);
+    });
+    $(".reset").on("click", function () {
+      $(".side__list__item").removeClass("active");
+    });
+    $("input").on("change", function () {
+      $("body").css("--pcolor", $(this).val());
+    });
+  });
+  
